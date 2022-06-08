165cd6440d0ebc6a136e99b472cf23af33386abb
rnjs010 <kyuri4809@naver.com>
2022-06-09 03:48:24 +0900

    first commit

--- DFS-BFS/BFS_1-2.py
+++ DFS-BFS/BFS_1-2.py
@@ -1,1 +1,36 @@
-# 음료수 얼려 먹기
+# 미로 탈출 - 너비우선탐색
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# 상 하 좌 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()     # 큐 생성
+    queue.append((x,y)) # 현재 위치 넣기
+
+    while queue:    # 큐가 비어 있지 않을 때까지
+        x, y = queue.popleft()  # 선입선출
+        for i in range(4):  # 모든 방향 확인
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:  # 그래프 벗어난 경우
+                continue
+
+            if graph[nx][ny] == 0:  # 장애물이 있는 경우
+                continue
+            
+            if graph[nx][ny] == 1:  # 처음 방문한 노드
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[n-1][m-1]  # 가장 오른쪽 아래의 값이 최단 거리
+
+print(bfs(0,0))
--- DFS-BFS/DFS_1-1.py
+++ DFS-BFS/DFS_1-1.py
@@ -1,1 +1,27 @@
-# 미로 탈출
+# 음료수 얼려 먹기 - 깊이우선탐색
+n, m = map(int, input().split())
+
+graph = []      # 공간을 그래프로 생각하고 2차원 리스트 생성
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:    # 벗어나면 False
+        return False
+    if graph[x][y] == 0:    # 방문하지 않은 곳
+        graph[x][y] = 1
+        
+        dfs(x-1, y) # 상 (주변 모두 확인)
+        dfs(x+1, y) # 하
+        dfs(x, y-1) # 좌
+        dfs(x, y+1) # 우
+        return True
+    return False
+
+count = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            count += 1
+
+print(count)
